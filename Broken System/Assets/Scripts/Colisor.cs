d23cf738a859a2db1d4d2d6ea94ee65427bf1620
CassianoRamos <30438171+CassianoRamos@users.noreply.github.com>
2018-07-14 21:27:58 -0300

    Control v2.4

---
+++ Broken System/Assets/Scripts/Colisor.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Colisor : MonoBehaviour {
+
+	void  OnTriggerEnter2D (Collider2D other){
+		if  (other.gameObject.CompareTag ( "Obstaculo")) {
+			string currentScene = SceneManager.GetActiveScene ().name;
+			SceneManager.LoadScene (currentScene);
+		}
+	}
+}
---
+++ Broken System/Assets/Scripts/Soundmanager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Soundmanager : MonoBehaviour {
+	public static Soundmanager Instance;
+
+	public AudioClip Laser1;
+	public AudioClip Saw1;
+
+	void Awake(){
+		Instance = this;
+	}
+	public void Laser1sound(){
+		Makesound (Laser1);
+	}
+	public void Saw1sound(){
+		Makesound (Saw1);
+	}
+	private void Makesound(AudioClip originalClip){
+		AudioSource.PlayClipAtPoint (originalClip, transform.position);
+	}
+}
